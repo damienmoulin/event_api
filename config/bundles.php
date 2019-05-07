da34a331c26fd4717d1a9bc0e4ee621d39db4fc6
damiemoulin <damien.moulin1@gmail.com>
2019-05-08 00:27:51 +0200

    [Config] Bundles

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],GIT
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
